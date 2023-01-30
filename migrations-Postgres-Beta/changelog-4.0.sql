e748178d9eea5a2df0f983463d1da44998945afe
Sergio Giraldo <sergio@giraldo.com.br>
2023-01-30 08:25:29 +0100

    testing liquibase hub:: by sergio giraldo @ 20230130T0825CET, gpg signed

--- migrations-Postgres-Beta/changelog-4.0.sql
+++ migrations-Postgres-Beta/changelog-4.0.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql changeLogId:a3f79240-ae84-45bc-8c36-afce577a00fa
 
 -- changeset sergiorgiraldo:4
---comment: i am using this changeset to test liquibase hub
+-- comment: i am using this changeset to test liquibase hub
 
 CREATE TABLE beta.users (id_user SERIAL PRIMARY KEY, str_name VARCHAR(256) NOT NULL);
 CREATE TABLE beta.roles (id_role SERIAL PRIMARY KEY, str_name VARCHAR(256) NOT NULL);
